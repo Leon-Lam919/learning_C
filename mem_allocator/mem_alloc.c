7347990f9fb2570ee88dbe67cd8b37869b145fb2
Leon Lam <leonlam34@gmail.com>
2024-10-22 20:43:08 -0500

    Created a notes file

--- mem_allocator/mem_alloc.c
+++ mem_allocator/mem_alloc.c
@@ -1,6 +1,13 @@
 // creating malloc and free from scratch
 
+#include <stdio.h>
+#include <stdlib.h>
+#define MEM_SPACE = 1024*1024 // 1 mb of space
 
+//bump allocator implementation
+void bump(){
+
+}
 
 // TODO: make function that calls sys's sbrk to allocate mem
 int main(){
