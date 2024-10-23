9f90879de97cec51a8528c88b31797837d525de0
Leon Lam <leonlam34@gmail.com>
2024-10-23 14:15:54 -0500

    creating bump allocator

--- mem_allocator/mem_alloc.c
+++ mem_allocator/mem_alloc.c
@@ -1,8 +1,11 @@
 // creating malloc and free from scratch
+#include <stdio.h>
+#include <unistd.h>
 
-
+// TODO: bump allocation fucntion
 
 // TODO: make function that calls sys's sbrk to allocate mem
-int main(){
+int main(int argc, char *argv[]){
+
 	return 0;
 }
