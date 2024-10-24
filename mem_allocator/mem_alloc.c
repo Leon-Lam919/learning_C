c6bac994df823e682d61703cf5e5f196910e09f7
Leon Lam <leonlam34@gmail.com>
2024-10-23 22:45:11 -0500

    MAHHHHHHHHHHHHHHH

--- mem_allocator/mem_alloc.c
+++ mem_allocator/mem_alloc.c
@@ -1,19 +1,17 @@
 // creating malloc and free from scratch
+
 #include <stdio.h>
 #include <unistd.h>
-
-<<<<<<< HEAD
-// TODO: bump allocation fucntion
-=======
-#include <stdio.h>
 #include <stdlib.h>
+
+// TODO: bump allocation fucntion
 #define MEM_SPACE = 1024*1024 // 1 mb of space
 
 //bump allocator implementation
-void bump(){
-
+void* bump(){
+	int mem[];
+	return mem[];
 }
->>>>>>> 1da0b374b6970c0bfeb1581e10510988cbf7f1b0
 
 // TODO: make function that calls sys's sbrk to allocate mem
 int main(int argc, char *argv[]){
