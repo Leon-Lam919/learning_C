37b2281c35b7a1548e7114f31debf4e6b1d835f0
Leon Lam <leonlam34@gmail.com>
2024-10-26 13:13:14 -0500

    finished mem alloc and now moving to web server

--- mem_allocator/mem_alloc.c
+++ mem_allocator/mem_alloc.c
@@ -4,17 +4,55 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-// TODO: bump allocation fucntion
-#define MEM_SPACE = 1024*1024 // 1 mb of space
+#define MEM_SPACE 1024*1024 // 1 mb of space
 
-//bump allocator implementation
-void* bump(){
-	int mem[];
-	return mem[];
+char memory_pool[MEM_SPACE];
+size_t current_offset = 0;
+
+// bump alloc function
+void* bump_alloc(size_t size){
+	if (current_offset + size > MEM_SPACE){
+		return NULL;
+	}
+
+	void* ptr = &memory_pool[current_offset];
+
+	current_offset += size;
+
+	return ptr;
 }
 
-// TODO: make function that calls sys's sbrk to allocate mem
-int main(int argc, char *argv[]){
-
-	return 0;
+//reset function
+void reset_bump(){
+	current_offset = 0;
 }
+
+//main
+int main() {
+    // Allocate memory
+    int* array1 = (int*)bump_alloc(10 * sizeof(int)); // Allocate 10 integers
+    if (array1 == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    // Use the allocated memory
+    for (int i = 0; i < 10; i++) {
+        array1[i] = i;
+        printf("%d ", array1[i]);
+    }
+    printf("\n");
+
+    // Allocate another block
+    char* array2 = (char*)bump_alloc(20 * sizeof(char)); // Allocate 20 chars
+    if (array2 == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    // Reset the memory pool
+    reset_bump();
+
+    return 0;
+}
+
---
+++ web_server/server.c
@@ -0,0 +1,10 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int main(){
+
+	int sockfd =  socket(AF_INET, SOCK_STREAM, 0);
+
+
+}
+
