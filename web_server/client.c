71f263589af8bee193c6f8ce8fc03eb1a4e20248
Leon Lam <leonlam34@gmail.com>
2024-11-06 19:21:47 -0600

    Multiple connections working :)

--- web_server/client.c
+++ web_server/client.c
@@ -5,8 +5,12 @@
 #include <stdlib.h> 
 #include <unistd.h> 
 #include <string.h>
+#include <pthread.h>
+
 
 #define BUF_SIZE 500
+
+// function that will create threads for each new client that tries to access server
 
 int main(int argc, char *argv[]) {
 
@@ -24,14 +28,11 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-
     /* Obtain address(es) matching host/port */
-
-
 
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-    hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+    hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
     hints.ai_flags = 0;
     hints.ai_protocol = 0;          /* Any protocol */
 
@@ -42,15 +43,11 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-
-
     /* getaddrinfo() returns a list of address structures.
 
        Try each address until we successfully connect(2).
        If socket(2) (or connect(2)) fails, we (close the socket
        and) try the next address. */
-
-
 
     for (rp = result; rp != NULL; rp = rp->ai_next) {
         sfd = socket(rp->ai_family, rp->ai_socktype,
@@ -58,35 +55,53 @@ int main(int argc, char *argv[]) {
         if (sfd == -1)
             continue;
 
-
-
         if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
             break;                  /* Success */
 
-
-
         close(sfd);
-
     }
-
 
     if (rp == NULL) {               /* No address succeeded */
         fprintf(stderr, "Could not connect\n");
         exit(EXIT_FAILURE);
     }
 
-
     freeaddrinfo(result);           /* No longer needed */
-
 
     /* Send remaining command-line arguments as separate
        datagrams, and read responses from server */
+    
+    while (1){
+        printf("Enter message (exit to quit): ");
+        fgets(buf, BUF_SIZE, stdin);
 
+        buf[strcspn(buf, "\n")] = '\0';
+
+        if(strcmp(buf, "exit") == 0){
+            printf("Exiting...\n");
+            break;
+        }
+
+        if (write(sfd, buf, strlen(buf)) == -1){
+            perror("write");
+            close(sfd);
+            exit(EXIT_FAILURE);
+        }
+
+        nread = read(sfd, buf, BUF_SIZE);
+        if (nread == - 1){
+            perror("read");
+            close(sfd);
+            exit(EXIT_FAILURE);
+        }
+
+        buf[nread] = '\0';
+        printf("Server reply: %s\n", buf);
+    }
 
     for (j = 3; j < argc; j++) {
         len = strlen(argv[j]) + 1;
                 /* +1 for terminating null byte */
-
 
         if (len + 1 > BUF_SIZE) {
             fprintf(stderr,
@@ -94,13 +109,11 @@ int main(int argc, char *argv[]) {
             continue;
         }
 
-
         if (write(sfd, argv[j], len) != len) {
 
             fprintf(stderr, "partial/failed write\n");
             exit(EXIT_FAILURE);
         }
-
 
         nread = read(sfd, buf, BUF_SIZE);
         if (nread == -1) {
@@ -108,10 +121,9 @@ int main(int argc, char *argv[]) {
             exit(EXIT_FAILURE);
         }
 
-
-
         printf("Received %zd bytes: %s\n", nread, buf);
     }
 
-
-    exit(EXIT_SUCCESS); }   
+    close(sfd);
+    exit(EXIT_SUCCESS); 
+} // main
--- web_server/server.c
+++ web_server/server.c
@@ -1,92 +1,124 @@
-#include <sys/types.h>
+
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <netdb.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define BUF_SIZE 500
+#define PORT 8080
 
-int main(int argc, char *argv[]){
-	
-	struct addrinfo hints;
-	struct addrinfo *result, *rp;
-	int sfd, s;
-	struct sockaddr_storage peer_addr;
-	socklen_t peer_addr_len;
-	ssize_t nread;
-	char buf[BUF_SIZE];
+// handles client messages/ bytes sent
+void handle_client(int client_sock){
+    char buffer[BUF_SIZE];
+    ssize_t nread;
+    while (1){
+	nread = read(client_sock, buffer, BUF_SIZE);
 
-	
-	if (argc != 2){
-		fprintf(stderr, "Usage: %s port \n", argv[0]);
-		exit(EXIT_FAILURE);
+	if (nread == -1){
+	    perror("read");
+	    close(client_sock);
+	    exit(EXIT_FAILURE);
+	} else if (nread == 0){
+	    printf("Client disconnected\n");
+	    break;
 	}
 
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;	// unspec is for IPv4 or IPv6
-	hints.ai_socktype = SOCK_DGRAM;	// datagram socket
-	hints.ai_flags = AI_PASSIVE;	// wildcard IP address
-	hints.ai_protocol = 0;		// any protocol
-	hints.ai_canonname = NULL;
-	hints.ai_addr = NULL;
-	hints.ai_next = NULL;
-	
+	buffer[nread] = '\0';
+	printf("Recvieved %zd bytes: %s\n", nread, buffer);
 
-	s = getaddrinfo(NULL, argv[1], &hints, &result);
-
-	if (s != 0){
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-		exit(EXIT_FAILURE);
+	if(write(client_sock, buffer, nread) != nread){
+	    perror("write");
+	    close(client_sock);
+	    exit(EXIT_FAILURE);
 	}
+    }
 
-	// getadddrinfo returns a list of addresses, try until we bind one
-	
-	for (rp = result; rp != NULL; rp = rp->ai_next){
-
-		// checks address until no address left to check
-		sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-		if (sfd == -1){
-			continue;
-		}
-		
-		// if bind works, break
-		if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0){
-			break;
-		}
-
-		close(sfd);
-	}
-
-	freeaddrinfo(result);
-
-	if (rp == NULL){
-		fprintf(stderr, "Could not bind\n");
-		exit(EXIT_FAILURE);
-	}
-
-	// read datagram and echos back to sender
-	for (;;){
-		peer_addr_len = sizeof(peer_addr);
-		nread = recvfrom(sfd, buf, BUF_SIZE, 0, (struct sockaddr*) &peer_addr, &peer_addr_len);
-		if (nread == -1){
-			continue;	
-		}
-
-		char host[NI_MAXHOST], service[NI_MAXSERV];
-		
-		s = getnameinfo((struct sockaddr *) &peer_addr, peer_addr_len, host, NI_MAXHOST, service, NI_MAXSERV, NI_NUMERICSERV);
-
-		if (s == 0){
-			printf("Recieved %zd bytes from %s:%s\n", nread, host, service);
-		} else{
-			fprintf(stderr, "getnameinfo: %s\n", gai_strerror(s));
-		}
-
-		if (sendto(sfd, buf, nread, 0, (struct sockaddr *) &peer_addr, peer_addr_len) != nread){
-			fprintf(stderr, "Error sending response.\n");
-		}
-	}
+    close(client_sock);
+    exit(EXIT_SUCCESS);
 }
 
+int main(int argc, char *argv[])
+{
+
+   int                      sfd, s, client_sock;
+   char                     buf[BUF_SIZE];
+   ssize_t                  nread, read_client;
+   socklen_t                peer_addrlen;
+   struct addrinfo          hints;
+   struct addrinfo          *result, *rp;
+   struct sockaddr_storage  peer_addr;
+   peer_addrlen = sizeof(peer_addr);
+
+   memset(&hints, 0, sizeof(hints));
+   hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+   hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+   hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+   hints.ai_protocol = 0;          /* Any protocol */
+   hints.ai_canonname = NULL;
+   hints.ai_addr = NULL;
+   hints.ai_next = NULL;
+
+   s = getaddrinfo(NULL, "8080", &hints, &result);
+   if (s != 0) {
+       fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+       exit(EXIT_FAILURE);
+   }
+
+   /* getaddrinfo() returns a list of address structures.
+      Try each address until we successfully bind(2).
+      If socket(2) (or bind(2)) fails, we (close the socket
+      and) try the next address. */
+
+
+   for (rp = result; rp != NULL; rp = rp->ai_next) {
+       sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+       if (sfd == -1)
+	   continue;
+
+       if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0)
+	   break;                  /* Success */
+
+       close(sfd);
+   }
+
+    printf("Listening on port %d...\n", PORT);
+   freeaddrinfo(result);           /* No longer needed */
+    
+    if (listen(sfd, SOMAXCONN) == -1){
+	perror("listen");
+	close(sfd);
+	exit(EXIT_FAILURE);
+    }
+
+   if (rp == NULL) {               /* No address succeeded */
+       fprintf(stderr, "Could not bind\n");
+       exit(EXIT_FAILURE);
+   }
+
+    while (1){
+	client_sock = accept(sfd, (struct sockaddr *)&peer_addr, &peer_addrlen);
+
+	if(client_sock < 0 ){
+	    perror("accept");
+	    continue;
+	}
+
+	pid_t pid = fork();
+	if (pid < 0){
+	    perror("fork");
+	    close(client_sock);
+	    continue;
+	} else if (pid == 0){
+	    close(sfd);
+	    handle_client(client_sock);
+	} else{
+	    close(client_sock);
+	}
+    }
+
+    close(sfd);
+    return 0;
+}
